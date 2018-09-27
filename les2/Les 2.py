6b1fc7c21e7afc6bc76e6cb05b22469420928b4e
mouadmakrini <33415946+mouadmakrini@users.noreply.github.com>
2018-09-27 10:50:18 +0200

    Les 2 opdrachten

---
+++ les2/Les 2.py
@@ -0,0 +1,24 @@
+Expressie	Uitkomst	type
+5		     5         Integer
+5.0		     5.0       Float
+5%2		     1        Integer
+5>1		     True     boolean
+‘5’	  	     ‘5’      String
+5*2		      10      Integer
+
+Strings
+1. len('Supercalifragilisticexpialidocious')
+2. ?
+3. (len('Antidisestablishmentarianism') > len('Honorificabilitudinitatibus'))
+True
+4.
+
+Statements
+1. a = 6 en b = 7
+2. c = 6.5
+3. d = 'Mouad' e = 'el' f = 'Makrini'
+4.mijnnaam = (d + ' ' + e + ' ' + f)
+
+Boolean expressions
+1. a < 75 < b
+2.
